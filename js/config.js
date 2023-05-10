cc8e1467620bf0b9f374f21bd79779d9596c51c0
Pratham Sharma <sharma.pratham0502@gmail.com>
2023-05-09 21:53:17 -0230

    changed url

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://90tucpep88.execute-api.ca-central-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://8llho6x6x4.execute-api.ca-central-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
